7af20ca23048c6e6131705ed9ddedacbd11d48ea
unknown <bsse0803@iit.du.ac.bd>
2020-08-03 19:55:14 +0600

    send parameter to python script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get("/", (req, res) => {
   var dataToSend;
   // spawn new child process to call the python script
-  const python = spawn("python", ["example.py"]);
+  const python = spawn("python", ["example.py","p1", "p2"]);
   // collect data from script
   python.stdout.on("data", function (data) {
     console.log("Pipe data from python script ...");
